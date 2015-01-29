e31f0d17d446b0b9d9f7c5ebeaa6b97e649c8946
Robert Schmidt <rjeschmi@gmail.com>
2015-01-29 10:38:17 -0500

    update to flash_id

--- flash_id.pl
+++ flash_id.pl
@@ -1,19 +1,37 @@
 #!/usr/bin/env perl
 
+my $LSSCSI="/software/packages/lsscsi-0.28b4r120/bin/lsscsi";
+my $SG_SES="/software/packages/sg3_utils-1.40b9r614/bin/sg_ses";
+
+my @SASs;
+
 while (<>) {
 	print "got line $_\n";
 	m/(0x[0-9a-f]+)/ or next;
-	my @lines = qx/lsscsi --wwn -t|grep $1/;
+	my @lines = qx/$LSSCSI --wwn -t|grep $1/;
 	$lines[0] =~ /sas:([0-9a-fx]+)/;
 	print "found sas: ".$1."\n";
-	my $DEV="sg20";
-	qx!sg_ses --sas-addr=$1 --set=ident /dev/$DEV 2>&1!;
+	push @SASs,$1;
+}
+
+foreach my $sasaddr(@SASs){
+	my $DEV="sg16";
+	qx!$SG_SES --sas-addr=$sasaddr --set=ident /dev/$DEV 2>&1!;
 	if ($?) {
 		print "exit code: ".$?."\n";
 		$DEV="sg37";
-		qx!sg_ses --sas-addr=$1 --set=ident /dev/$DEV 2>&1!;
+		qx!$SG_SES --sas-addr=$sasaddr --set=ident /dev/$DEV 2>&1!;
 	}
-	sleep 2;
-	qx!sg_ses --sas-addr=$1 --clear=ident /dev/$DEV 2>&1!;
-}
+} 
 
+sleep 50;
+
+foreach my $sasaddr (@SASs){
+	my $DEV = "sg16";
+	qx!$SG_SES --sas-addr=$sasaddr --clear=ident /dev/$DEV 2>&1!;
+	if ($?) {
+		$DEV="sg37";
+		qx!$SG_SES --sas-addr=$sasaddr --clear=ident /dev/$DEV 2>&1!;
+	}
+
+} 
