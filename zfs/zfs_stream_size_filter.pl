817375fb2109b668ee8739c4e8d72f1b28d626c0
Robert Schmidt <rjeschmi@gmail.com>
2016-02-12 14:43:08 -0500

    a zfs stream size filter in perl

--- zfs/zfs_stream_size_filter.pl
+++ zfs/zfs_stream_size_filter.pl
@@ -3,18 +3,36 @@
 #
 
 use strict;
+use Redis;
+use Storable qw(freeze) ;
+use Data::Printer;
+
+
 my $last = {};
+my $stats = {};
+
+my $redis = Redis->new;
 
 while (<>) {
 
 	if(/^(FREE|WRITE|OBJECT)\sobject\s=\s(\d+)/) {
-	    if($last->{id} != $2) {
+	    if($last->{id} != $2 && $last->{id}) {
+            #print "adding ".$last->{id}." ".$last->{sum}." ".freeze($last)." to redis\n";
+            $redis->zadd("stats", $last->{sum}, freeze($last));
+            print "A scan: \n";
+            p $redis->zscan("stats", 0);
             print join ("\t", ($last->{id}, $last->{cmd}, $last->{sum}))."\n";
-		    print "zdb -dddd bioinfo2/backup\@2015-07-27-000000 $2\n";
+		    #print "zdb -dddd bioinfo2/backup\@2015-07-27-000000 $2\n";
 	        $last->{cmd}=$1;
             $last->{id} =$2;
             $last->{sum} = 0;
 	    }
+        elsif (!defined($last->{id})) {
+            print "setting last\n";
+            $last->{cmd}=$1;
+            $last->{id} =$2;
+            $last->{sum} = 0;
+        }
 
 	}
     elsif(/^offset = (\d+) length = (\d+)/ ) {
@@ -25,3 +43,6 @@ while (<>) {
 	}
 
 }
+
+
+
